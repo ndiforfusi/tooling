0e7e381a2ebca5bf0c67b85569da2f5949f72825
mrswalter404 <mrswalter404@yahoo.com>
2025-05-16 23:01:43 -0500

    modified s3 & dynamodb

--- modules/s3-dynamodb/variables.tf
+++ modules/s3-dynamodb/variables.tf
@@ -1,17 +1,22 @@
 variable "bucket" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "class38-terraform-backend-bucket"
+  default     = "class38-terraform-backend-bucket-01"
+  #default     = "terraform-state-bucket-fusi"
+ 
 }
 
-variable "table" {
+
+variable "DynamoDBtable" {
   description = "The name of the DynamoDB table"
   type        = string
-  default     = "terraform-state-locking"
+  default     = "terraform-state-file-locking"
+
+  #default     = "terraform-state-locking"
 }
 
 variable "region" {
   description = "The AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-west-1"
 }
