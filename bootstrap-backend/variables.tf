96bd68275510b30b5209ac7dc2c60a11f72deb65
Sallieu-99 <sallieusesay441@gmail.com>
2025-06-03 21:35:35 +0200

    Update variables.tf

--- bootstrap-backend/variables.tf
+++ bootstrap-backend/variables.tf
@@ -1,7 +1,7 @@
 variable "bucket" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "class38-terraform-backend-bucket-project"
+  default     = "class38-terraform-backend-bucket-project-sal"
 }
 
 variable "table" {
