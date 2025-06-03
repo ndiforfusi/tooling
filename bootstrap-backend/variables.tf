83a5ced9559f204cd443bcf61ad7eea1fb6458e5
Sallieu-99 <sallieusesay441@gmail.com>
2025-06-03 20:13:40 +0200

    Create variables.tf

---
+++ bootstrap-backend/variables.tf
@@ -0,0 +1,17 @@
+variable "bucket" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "class38-terraform-backend-bucket-project"
+}
+
+variable "table" {
+  description = "The name of the DynamoDB table"
+  type        = string
+  default     = "terraform-state-locking"
+}
+
+variable "region" {
+  description = "The AWS region"
+  type        = string
+  default     = "us-east-2"
+}
